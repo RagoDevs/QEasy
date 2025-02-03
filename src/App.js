09ddaa50871eff24b3e10b2caa79bef0b44cbe14
Jerry John <148745725+RagoTheDev@users.noreply.github.com>
2025-02-03 10:16:39 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './Pages/Dashboard/Dashboard';
-import { Routes, Route } from 'react-router-dom'
 import Login from './Pages/Login/Login';
 
 function App() {
   return (
-    <Routes>
-      <Route 
-        path=''
-        element={
-          <Login />
-        }
-      />
-      <Route 
-        path='dashboard'
-        element={
-          <Dashboard />
-        }
-      />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
